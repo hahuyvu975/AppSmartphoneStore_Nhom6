d91d3e69cbe4a54f9a45626ff2c1d37712cb846f
hahuyvu975 <hahuyvu975@gmail.com>
2022-11-21 22:11:07 +0700

    Da hoan thanh Subproject LTTBDD

---
+++ src/consts/categories.js
@@ -0,0 +1,8 @@
+const categories = [
+    {id: '1', name: 'Iphone', image: require('../assets/logoIp.png')},
+    {id: '2', name: 'SamSung', image: require('../assets/logoss.png')},
+    {id: '3', name: 'Oppo', image: require('../assets/logoOppo.png')},
+    {id: '4', name: 'Xiaomi', image: require('../assets/logoXiaomi.png')},
+  ];
+  
+  export default categories;
---
+++ src/consts/phones.js
@@ -0,0 +1,32 @@
+const phones = [
+    {
+      id: '1',
+      name: 'Iphone 14 Pro Max',
+      ingredients: '6.7 inch, Super Retina XDR',
+      price: '1650',
+      image: require('../assets/ip2.png'),
+    },
+    {
+      id: '2',
+      name: 'Samsung Galaxy S22',
+      ingredients: '6.1 inch, Dynamic AMOLED 2X',
+      price: '1350',
+      image: require('../assets/ss1.png'),
+    },
+    {
+      id: '3',
+      name: 'OPPO Reno8 4G',
+      ingredients: '6.4 inch, Chính: AMOLED',
+      price: '950',
+      image: require('../assets/op1.png'),
+    },
+    {
+      id: '4',
+      name: 'Xiaomi 12T Pro',
+      ingredients: '6.67 inch, CrystalRes AMOLED',
+      price: '800',
+      image: require('../assets/xm1.png'),
+    },
+  ];
+  
+  export default phones;
